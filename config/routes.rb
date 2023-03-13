731ebdd7aa4aff760ecd831511af437048cca805
Vinleon Ang <javadripdev@gmail.com>
2023-03-13 16:26:41 +0800

    Add bids routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  resources :jobs, only: %i[index show new create]
+  resources :jobs, only: %i[index show new create] do
+    resources :bids, only: %i[index new create update]
+  end
 
   get 'freelancers/', to: 'users#index', as: :freelancers
   get 'freelancers/:id', to: 'users#show', as: :freelancer
