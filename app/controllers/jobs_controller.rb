dec7d9ae6b027db87d98425809d8166ecaef56de
Vinleon Ang <javadripdev@gmail.com>
2023-03-13 15:00:18 +0800

    Install gems and add jobs controller

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,22 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
