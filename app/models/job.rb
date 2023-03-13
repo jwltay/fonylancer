1043ff9985b3d73dac4b95f9af795db6de90a196
deepika joshi <upreti.deepika@gmail.com>
2023-03-13 16:45:29 +0800

    Added relations to all models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
-  belongs_to :User
+  belongs_to :user, class_name: "User", foreign_key: :employer_id
+  has_many :users, through: :bids
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :bids
+  has_many :jobs, through: :bids
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
