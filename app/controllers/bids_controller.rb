9365350c2dc257406436ec03457825f3c397bbc2
Leanne Tay <duckingville@yahoo.com>
2023-03-13 15:12:45 +0800

    added #create and #bid_params to bids controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,4 +8,20 @@ class BidsController < ApplicationController
     @job = Job.find(params[:job_id])
     @bid = Bid.new
   end
+
+  def create
+    @job = Job.find(params[:jobs_id])
+    bid = Bid.new(job: @job, user: bid_params[:user], rate: bid_params[:rate])
+    if bid.save
+      redirect_to job_path(@job)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bid_params
+    params.require(:bid).permit(:user, :rate)
+  end
 end
