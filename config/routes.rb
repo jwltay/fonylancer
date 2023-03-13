e2d15c6074289d86c4fa0281cc755ebd132f520a
Vinleon Ang <javadripdev@gmail.com>
2023-03-13 16:23:06 +0800

    Redefine freelancer user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :jobs, only: %i[index show new create]
-  resources :users, only: %i[index show]
+
+  get 'freelancers/', to: 'users#index', as: :freelancers
+  get 'freelancers/:id', to: 'users#show', as: :freelancer
 end
