3986faadfcb3ea6c5d9b431e22904ddb49341976
Vinleon Ang <javadripdev@gmail.com>
2023-03-13 15:58:34 +0800

    Add jobs and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :jobs, only: %i[index show new create]
+  resources :users, only: %i[index show]
 end
