c157af76e6df5c504bba3a91542374258ac56713
Leanne Tay <duckingville@yahoo.com>
2023-03-13 15:14:49 +0800

    refactored method to find current job into private #set_jobs

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,16 +1,15 @@
 class BidsController < ApplicationController
+  before_action :set_job
+
   def index
-    @job = Job.find(params[:job_id])
     @bids = @job.bids
   end
 
   def new
-    @job = Job.find(params[:job_id])
     @bid = Bid.new
   end
 
   def create
-    @job = Job.find(params[:jobs_id])
     bid = Bid.new(job: @job, user: bid_params[:user], rate: bid_params[:rate])
     if bid.save
       redirect_to job_path(@job)
@@ -23,5 +22,9 @@ class BidsController < ApplicationController
 
   def bid_params
     params.require(:bid).permit(:user, :rate)
+  end
+
+  def set_job
+    @job = Job.find(params[:job_id])
   end
 end
