9f1fa19ff06fe673522977eadaede8b03e6b6262
Leanne Tay <duckingville@yahoo.com>
2023-03-13 14:59:34 +0800

    added #new to bids controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,4 +3,9 @@ class BidsController < ApplicationController
     @job = Job.find(params[:job_id])
     @bids = @job.bids
   end
+
+  def new
+    @job = Job.find(params[:job_id])
+    @bid = Bid.new
+  end
 end
