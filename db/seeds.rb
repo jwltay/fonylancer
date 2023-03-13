b7bf36b2791258bc1fea7703cb77f03f422621a8
Vinleon Ang <javadripdev@gmail.com>
2023-03-13 17:38:28 +0800

    Add seed generator to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  User.create!(
+    username: Faker::Internet.username,
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    description: Faker::Quote.famous_last_words,
+    location: Faker::Address.full_address,
+    tagline: Faker::Marketing.buzzwords
+  )
+end
