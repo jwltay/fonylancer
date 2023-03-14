36e364fe367bc4e7815098e9701860bfcc6e8c11
Leanne Tay <duckingville@yahoo.com>
2023-03-14 21:28:52 +0800

    profile bar appears after scrolling down but doesnt disappear after scrollign up

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import ProfileBarController from "./profile_bar_controller"
-application.register("profile-bar", ProfileBarController)
+import ProfilebarController from "./profilebar_controller"
+application.register("profilebar", ProfilebarController)
