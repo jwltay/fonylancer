cf557b25276180d6249f68563d4a222fffa99140
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-15 23:40:52 +0800

    Generated user_policy and logged in user can now browse freelancers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @freelancers = User.where(want_to_work: true).paginate(page: params[:page], per_page: 5)
+    @freelancers = policy_scope(User).paginate(page: params[:page], per_page: 5)
   end
 
   def show
