111493d45c5d94828025d1959aba00fe5b49cbeb
Leanne Tay <duckingville@yahoo.com>
2023-03-16 20:40:10 +0800

    added user method #open_projects that finds all jobs still pending for that user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @successful_bids = Bid.where(freelancer: @user, accepted: true)
+    @successful_closed_bids = Bid.where(freelancer: @user, accepted: true)
     authorize @user
   end
 end
